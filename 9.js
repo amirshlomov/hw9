f7e582e3ae60ff2d58698ff10a81c76c76fa0d32
amirshlomov <amir.shlomov@gmail.com>
2025-01-18 12:47:56 +0200

    Update 9.js

--- 9.js
+++ 9.js
@@ -9,3 +9,20 @@ alert(c + warEnd)
 
 
 alert("המלחמה נמשכה " + (warEnd-warStart) + " שנים")
+
+
+function checkQuestion(name) {
+    var ele = document.getElementsByName(name);
+    let answerSelected = false;
+    //עבור על כל הרדיו והדפס את הערך של התשובה שנבחרה
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            alert(ele[i].value);
+            answerSelected = true;
+        }
+    }
+    //בדוק אם תשובה נבחרה
+    if (answerSelected == false) {
+        alert("בחר תשובה בבקשה");
+    }
+}
